57279f27aa617ea39d9430323ec52898155dfb74
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-31 18:09:57 +0530

    Add Categories Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react';
 import Title from './Title';
 import menu from './data';
 import Menu from './Menu';
+import Categories from './Categories';
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu"></Title>
+        <Categories categories={categories}></Categories>
         <Menu items={menuItems}></Menu>
       </section>
     </main>
